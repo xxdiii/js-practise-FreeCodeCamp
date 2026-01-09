b120add5a707cb0728382c30da6b5c6afe9a2a98
Aadithya <asadithya26@gmail.com>
2026-01-09 17:04:47 +0530

    completed all true propert validator

---
+++ 25.All-True Property Validator/script.js
@@ -0,0 +1,10 @@
+function truthCheck(collection, pre) {
+   return collection.every(element=>{
+    if(!element[pre]){
+        return false;
+    }
+    return true;
+   })
+}
+
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
