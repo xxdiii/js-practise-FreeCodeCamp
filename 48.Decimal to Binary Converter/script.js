00b50d7a1b0fc58f03d8c7eca9029db5ba27b480
Aadithya <asadithya26@gmail.com>
2026-01-31 13:23:41 +0530

    completed decimal to binary converter

---
+++ 48.Decimal to Binary Converter/script.js
@@ -0,0 +1,29 @@
+const numberInput=document.getElementById("number-input");
+const convertBtn=document.getElementById("convert-btn");
+const result=document.getElementById("result");
+
+const checkUserInput=()=>{
+    if(!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value)<0){
+        alert("Please provide a decimal number greater than or equal to 0");
+        return;
+    }
+    result.textContent=decimalToBinary(parseInt(numberInput.value));
+    numberInput.value="";
+}
+
+convertBtn.addEventListener("click",checkUserInput);
+
+numberInput.addEventListener("keydown",(e)=>{
+    if(e.key==="Enter"){
+        checkUserInput();
+    }  
+})
+
+const decimalToBinary=(input)=>{
+    if(input===0 || input===1){
+        return String(input);
+    }
+    else{
+        return decimalToBinary(Math.floor(input/2))+(input%2);
+    }
+};
